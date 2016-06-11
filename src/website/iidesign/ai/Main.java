05aaa46e4a2f5cadb74f06c97784944a7c2d1d8d
kotaroyamame <yamame@fi.chu.jp>
2016-06-10 20:35:39 -0400

    学習結果表示

--- src/website/iidesign/ai/Main.java
+++ src/website/iidesign/ai/Main.java
@@ -30,7 +30,7 @@ public class Main {
 		n[0].setWeight(0,0);
 		
 		//学習回数
-		for(int l=0;l<10;l++)
+		for(int l=0;l<30;l++)
 		for(int i=0;i<x.length;i++){
 			System.out.println("net="+n[0].hog(x[i][0],x[i][1])+"y="+y[i]);
 		if(n[0].hog(x[i][0],x[i][1])==y[i]){
@@ -38,15 +38,20 @@ public class Main {
 		}else{
 			System.out.println("W値修正必要あり");
 			if(n[0].hog(x[i][0],x[i][1])-y[i]>0)
-				newW=new double[]{(n[0].getWeight()[0]-x[i][0]*eta),(n[0].getWeight()[1]-x[i][1]*eta)};
+				newW=new double[]{(n[0].getWeight()[0]-(1+x[i][0]*eta)),(n[0].getWeight()[1]-(1+x[i][1]*eta))};
 			else
-				newW=new double[]{(n[0].getWeight()[0]+x[i][0]*eta),(n[0].getWeight()[1]+x[i][1]*eta)};
+				newW=new double[]{(n[0].getWeight()[0]+(1+x[i][0]*eta)),(n[0].getWeight()[1]+(1+x[i][1]*eta))};
 			n[0].setWeight(newW[0],newW[1]);
 		}
 		System.out.println("net="+n[0].hog(x[i][0],x[i][1])+"y="+y[i]);
 		}
 		for(int j=0;j<n[0].getWeight().length;j++)
 			System.out.println("w="+n[0].getWeight()[j]);
+		
+		System.out.println("net="+n[0].hog(0,0)+" 0,0");
+		System.out.println("net="+n[0].hog(0,1)+" 0,1");
+		System.out.println("net="+n[0].hog(1,0)+" 1,0");
+		System.out.println("net="+n[0].hog(1,1)+" 1,1");
 		
 		
 	}
